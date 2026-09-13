570c561d7ba162e62b0841b1fcd16ab018a73f03
afiq <mohamadafiq126@outlook.com>
2026-09-13 22:54:55 +0800

    outputs.tf add private ip outputs for all servers

--- iac/aws/outputs.tf
+++ iac/aws/outputs.tf
@@ -1,5 +1,20 @@
 output "webserver_ip" { value = module.webserver.public_ip }
 //output "ctrl_ip" { value = module.ctrl.public_ip }
+output "web_private_ip" {
+  value = module.web.private_ip
+}
+
+output "web_elastic_ip" {
+  value = module.webserver.public_ip
+}
+
+output "controller_private_ip" {
+  value = module.ctrl.private_ip
+}
+
+output "monitoring_private_ip" {
+  value = module.mon.private_ip
+}
 
 output "ssm_command_1" {
   value = "aws ssm start-session --target ${module.webserver.id}"
