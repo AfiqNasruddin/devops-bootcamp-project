8cd0697cfa7d9fd6a06715d7aa81d1b05745971c
afiq <mohamadafiq126@outlook.com>
2026-09-14 01:58:37 +0800

    Repair network.tf

--- iac/aws/network.tf
+++ iac/aws/network.tf
@@ -2,7 +2,7 @@ module "my_vpc"{
     source = "terraform-aws-modules/vpc/aws"
     version = "~> 6.0"
 
-    name = "vpc-devops"
+    name = "devops-vpc"
     cidr = var.cidr_block_my_vpc
     azs = [var.az]
     public_subnets  = [var.subnet_cidr_block_public]
@@ -12,14 +12,14 @@ module "my_vpc"{
     single_nat_gateway      = true
     enable_vpn_gateway      = false
 
-    //igw_tags                 = { Name = "devops-igw" }
-    //nat_gateway_tags         = { Name = "devops-ngw" }
-    //public_subnet_tags       = { Name = "devops-public-subnet" }
-    //private_subnet_tags      = { Name = "devops-private-subnet" }
-    //public_route_table_tags  = { Name = "devops-public-route" }
-    //private_route_table_tags = { Name = "devops-private-route" }
+    igw_tags                 = { Name = "devops-igw" }
+    nat_gateway_tags         = { Name = "devops-ngw" }
+    public_subnet_tags       = { Name = "devops-public-subnet" }
+    private_subnet_tags      = { Name = "devops-private-subnet" }
+    public_route_table_tags  = { Name = "devops-public-route" }
+    private_route_table_tags = { Name = "devops-private-route" }
 
-    tags = { Name = "vpc-devops" }
+    tags = { Name = "devops-vpc" }
     
 }
 
