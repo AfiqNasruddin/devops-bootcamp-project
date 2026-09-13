864513262019fa02c616ea9f445c5579117a350d
afiq <mohamadafiq126@outlook.com>
2026-09-13 22:30:17 +0800

    security.tf: Fix each ingress rule must set exactly one of cidr_ipv4, cidr_ipv6, prefix_list_id, or referenced_security_group_id.

--- iac/aws/security.tf
+++ iac/aws/security.tf
@@ -6,15 +6,22 @@ module "public_sg" {
   use_name_prefix = false
   vpc_id          = module.my_vpc.vpc_id
 
+
   ingress_rules = {
     http = {
       cidr_ipv4   = "10.0.0.136/32"
+      cidr_ipv6   = false
+      prefix_list_id = false
+      reference_security_group_id = false
       ip_protocol = "tcp"
       from_port   = 9100
       to_port     = 9100
     }
     ssh = {
       cidr_ipv4   = var.subnet_cidr
+      cidr_ipv6   = false
+      prefix_list_id = false
+      reference_security_group_id = false
       ip_protocol = "tcp"
       from_port   = 22
       to_port     = 22
@@ -37,6 +44,9 @@ module "private_sg" {
   ingress_rules = {
     ssh = {
       cidr_ipv4   = var.subnet_cidr
+      cidr_ipv6   = false
+      prefix_list_id = false
+      reference_security_group_id = false
       ip_protocol = "tcp"
       from_port   = 22
       to_port     = 22
