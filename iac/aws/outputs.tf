c2ec3cab85bbb5838a72a1ceaf1b348b007c4ef6
afiq <mohamadafiq126@outlook.com>
2026-09-13 22:21:35 +0800

    repair inventory.tf and outputs.tf

--- iac/aws/outputs.tf
+++ iac/aws/outputs.tf
@@ -1,10 +1,14 @@
-output "node1_ip" { value = module.node1.public_ip }
-output "node2_ip" { value = module.node2.public_ip }
+output "web_ip" { value = module.web.public_ip }
+//output "ctrl_ip" { value = module.ctrl.public_ip }
 
 output "ssm_command_1" {
-  value = "aws ssm start-session --target ${module.node1.id}"
+  value = "aws ssm start-session --target ${module.web.id}"
 }
 
 output "ssm_command_2" {
-  value = "aws ssm start-session --target ${module.node2.id}"
+  value = "aws ssm start-session --target ${module.ctrl.id}"
+}
+
+output "ssm_command_3" {
+  value = "aws ssm start-session --target ${module.mon.id}"
 }
