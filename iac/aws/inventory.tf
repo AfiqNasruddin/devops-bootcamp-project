a087f53b2eac349a920268aaaa5eada8336b2275
afiq <mohamadafiq126@outlook.com>
2026-09-14 23:22:05 +0800

    inventory.tf: fix webserver public ip to private ip

--- iac/aws/inventory.tf
+++ iac/aws/inventory.tf
@@ -1,7 +1,7 @@
 resource "local_file" "inventory" {
   filename = "inventory.ini"
   content = templatefile("inventory.ini.tftpl", {
-    webserver_ip = module.webserver.public_ip
+    webserver_ip = module.webserver.private_ip
     ctrl_ip = module.ctrl.private_ip
     mon_ip = module.mon.private_ip
   })
